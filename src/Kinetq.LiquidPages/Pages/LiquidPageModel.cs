49ada53e0a1d766a6728ffd7ee02f4bedc4c5767
ssinno28 <sammi.sinno@gmail.com>
2026-05-25 09:56:07 -0400

    defaulting file provider to PhysicalFileProvider

--- src/Kinetq.LiquidPages/Pages/LiquidPageModel.cs
+++ src/Kinetq.LiquidPages/Pages/LiquidPageModel.cs
@@ -7,15 +7,9 @@ public abstract class LiquidPageModel
 {
     public virtual IFileProvider GetFileProvider()
     {
-#if DEBUG
         string workingDirectory = Directory.GetCurrentDirectory();
         string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
         return new PhysicalFileProvider(projectDirectory);
-#endif
-
-#if !DEBUG
-        return new EmbeddedFileProvider(GetType().Assembly, GetType().Namespace);
-#endif
     }
     public virtual Task OnGetAsync(LiquidRequestModel request) => Task.CompletedTask;
     public virtual Task OnPostAsync(LiquidRequestModel request) => Task.CompletedTask;
